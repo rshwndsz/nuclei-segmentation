ff51d59b6d2d085c50f04d2e685937b4b955c61f
Russel Dsouza <russel.dsouza47@gmail.com>
2019-07-02 20:02:11 +0530

    Add validation and testing loops

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import torch
 import argparse
+import os
+from torchvision import transforms as T
 import config as cfg
-import torch.nn.functional as F
 
 
 def train(model):
     # TODO: Add ability to load from a checkpoint save
     model.to(cfg.device)
+    model.train()
     optimizer = cfg.optimizer
     criterion = cfg.criterion
     train_loader = cfg.train_loader
@@ -19,8 +21,7 @@ def train(model):
             print('Mem Cached:', torch.cuda.memory_cached(0))
 
             sample['image'] = sample['image'].to(cfg.device)
-            sample['label'] = sample['label'].type(torch.FloatTensor).to(cfg.device)
-            print('sample label dtype:', sample['label'].type())
+            sample['label'] = sample['label'].to(cfg.device)
 
             prediction = model(sample['image'])
             loss = criterion(prediction, sample['label'])
@@ -33,12 +34,36 @@ def train(model):
             print('Epoch done! Loss =', running_loss/len(train_loader))
 
 
-def val():
-    pass
+def val(model):
+    model.to(cfg.device)
+    model.eval()
+    criterion = cfg.criterion
+    val_loader = cfg.val_loader
+
+    running_loss = 0
+    for sample in val_loader:
+        sample['image'] = sample['image'].to(cfg.device)
+        sample['label'] = sample['label'].to(cfg.device)
+
+        prediction = model(sample['image'])
+        loss = criterion(prediction, sample['label'])
+
+        running_loss += loss.item()
+    else:
+        print('Validation Loss: {}'.format(running_loss/len(val_loader)))
 
 
-def test():
-    pass
+def test(model):
+    model.to(cfg.device)
+    model.eval()
+
+    test_loader = cfg.test_loader
+    for sample in test_loader:
+        sample['image'] = sample['image'].to(cfg.device)
+
+        prediction = model(sample['image'])
+        T.ToPILImage(prediction).save(os.path.join(cfg.results_dir, 'output.jpeg'))
+        print('Prediction saved in results/output.jpeg')
 
 
 if __name__ == '__main__':

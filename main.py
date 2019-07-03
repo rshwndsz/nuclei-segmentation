11f6c8b3deca7bae08418386e848cc45e2c80bd7
rshwndsz <russel.dsouza47@gmail.com>
2019-07-03 13:07:40 +0530

    Fix training loop and add todos

--- main.py
+++ main.py
@@ -1,43 +1,60 @@
 import torch
 import argparse
-import config as cfg
-import torch.nn.functional as F
+from config import config as cfg
+import logging
+import coloredlogs
+
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='INFO', logger=logger)
 
 
 def train(model):
     # TODO: Add ability to load from a checkpoint save
+    # See: https://pytorch.org/tutorials/beginner/saving_loading_models.html
     model.to(cfg.device)
     optimizer = cfg.optimizer
     criterion = cfg.criterion
     train_loader = cfg.train_loader
 
     for e in range(cfg.n_epochs):
-        print('Epoch {}/{}'.format(e+1, cfg.n_epochs))
+        logger.info('Epoch {}/{}'.format(e+1, cfg.n_epochs))
         running_loss = 0
+        step = 0
         for sample in train_loader:
-            print('Mem Used:', torch.cuda.memory_allocated(0))
-            print('Mem Cached:', torch.cuda.memory_cached(0))
-
+            step += 1
+            logger.info(f'Image {step}/{len(train_loader)}')
             sample['image'] = sample['image'].to(cfg.device)
-            sample['label'] = sample['label'].type(torch.FloatTensor).to(cfg.device)
-            print('sample label dtype:', sample['label'].type())
+            sample['label'] = sample['label'].type(torch.LongTensor).to(cfg.device)
 
-            prediction = model(sample['image'])
-            loss = criterion(prediction, sample['label'])
+            loss = criterion(model(sample['image']), sample['label'])
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
         else:
-            print('Epoch done! Loss =', running_loss/len(train_loader))
+            logger.info(f'Epoch done! Loss = {running_loss/len(train_loader)}')
+            logger.info('Saving model...')
+            try:
+                torch.save({
+                    'epoch': e,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': running_loss,
+                }, cfg.model_path)
+            except FileNotFoundError as fnf_error:
+                logger.error(f'Unable to save.: {fnf_error}')
+            else:
+                logger.info('Saved 🎉')
 
 
 def val():
+    # TODO: Implement this from 'refactor'
     pass
 
 
 def test():
+    # TODO: Implement this from 'refactor'
     pass
 
 
@@ -56,4 +73,4 @@ if __name__ == '__main__':
         val()
 
     else:
-        raise ValueError('Choose one of train/validate/test')
+        raise ValueError('Choose one of train/validate/test.')

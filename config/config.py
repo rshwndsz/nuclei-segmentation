b7981d848853620289a42474a98a761c1339a634
rshwndsz <russel.dsouza47@gmail.com>
2019-07-03 13:05:29 +0530

    Convert to Py modules

--- config/config.py
+++ config/config.py
@@ -1,14 +1,13 @@
 import os
 import torch
-import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 
 # Torch-specific
 use_gpu = torch.cuda.is_available()
 device = torch.device('cuda' if use_gpu else 'cpu')
-num_workers = 0
+num_workers = 4
 print_freq = 20
-load_model_path = 'checkpoints/model.pth'
 
 # Hyper-parameters
 batch_size = 1
@@ -18,14 +17,16 @@ lr = 0.01
 # Architecture-specific
 from models import UNet
 model_name = 'UNet'
-n_classes = 3
+n_classes = 2
 model = UNet(n_classes)
-criterion = nn.CrossEntropyLoss()
+criterion = F.cross_entropy
 optimizer = optim.Adam(model.parameters())
 
 # Data-specific
-project_root = os.getcwd()
+# TODO: Make project_root platform agnostic
+project_root = "E:\\project_russel\\unet"
 dataset_root = os.path.join(project_root, 'datasets', 'kidney')
+model_path = os.path.join(project_root, 'checkpoints', 'model.pth')
 
 from data import kidney
 train_loader = kidney.train_loader

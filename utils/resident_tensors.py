ad47e064549960777cf875a8aeedaf85a29be7e3
rshwndsz <russel.dsouza47@gmail.com>
2019-07-04 15:44:41 +0530

    Add script to output resident tensors

---
+++ utils/resident_tensors.py
@@ -0,0 +1,12 @@
+import torch
+import gc
+
+objs = 0
+for obj in gc.get_objects():
+    try:
+        if torch.is_tensor(obj) or (hasattr(obj, 'data') and torch.is_tensor(obj.data)):
+            logger.info(f'{type(obj)}, {obj.size()}')
+            objs += 1
+    except:
+        pass
+logger.info(f'Number of resident tensors: {objs}')

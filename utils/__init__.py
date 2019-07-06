836a1d8601af4b3e253a3b1375d8749d261c9b99
Russel Dsouza <russel.dsouza47@gmail.com>
2019-07-06 17:44:46 +0530

    Refactor resident tensors

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
 from . import PIL_helpers
 from . import train_helpers
+from .resident_tensors import get_resident_tensor
--- utils/resident_tensors.py
+++ utils/resident_tensors.py
@@ -1,12 +1,14 @@
 import torch
 import gc
 
-objs = 0
-for obj in gc.get_objects():
-    try:
-        if torch.is_tensor(obj) or (hasattr(obj, 'data') and torch.is_tensor(obj.data)):
-            logger.info(f'{type(obj)}, {obj.size()}')
-            objs += 1
-    except:
-        pass
-logger.info(f'Number of resident tensors: {objs}')
+
+def get_resident_tensor(logger):
+    objs = 0
+    for obj in gc.get_objects():
+        try:
+            if torch.is_tensor(obj) or (hasattr(obj, 'data') and torch.is_tensor(obj.data)):
+                logger.info(f'{type(obj)}, {obj.size()}')
+                objs += 1
+        except:
+            pass
+    logger.info(f'Number of resident tensors: {objs}')

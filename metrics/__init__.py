09ee46d9b429a3a41a64ee52d58acf62180be1c8
Russel Dsouza <russel.dsouza47@gmail.com>
2019-07-08 20:07:20 +0530

    Add accuracy score metric

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,2 +1,5 @@
-from .numeric_score import numeric_score
+from .numeric_score import numeric_score, accuracy_score
 from .dice_coeff import dice_coeff
+
+# See https://github.com/eladhoffer/utils.pytorch/blob/74d07a5c7fb727075d0397827dbc6b979015aad7/meters.py
+# for more metrics
--- metrics/numeric_score.py
+++ metrics/numeric_score.py
@@ -18,3 +18,17 @@ def numeric_score(prediction, ground_truth):
     tn = np.float(np.sum((prediction == 0) & (ground_truth == 0)))
 
     return fp, fn, tp, tn
+
+
+def accuracy_score(prediction, ground_truth):
+    """
+    Computes model accuracy
+
+    :param prediction: Predicted Tensor
+    :param ground_truth: Ground truth Tensor
+    :return: Accuracy in percentage
+    """
+    fp, fn, tp, tn = numeric_score(prediction, ground_truth)
+    n = fp + fn + tp + tn
+    accuracy = np.divide(tp + tn, n)
+    return accuracy * 100.0

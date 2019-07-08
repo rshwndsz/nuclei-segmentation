92a94251bdef0d641f388303d1bc7824756f2fd5
Russel Dsouza <russel.dsouza47@gmail.com>
2019-07-08 17:00:57 +0530

    Add basic Augmentation, Normalization
    * Also add link to extra augmentation

--- data/augmentation.py
+++ data/augmentation.py
@@ -1,1 +1,12 @@
-# TODO: Add data augmentation
+import torchvision.transforms as T
+
+augmentation = T.Compose([
+    T.RandomRotation(30),
+    T.RandomHorizontalFlip(),
+    T.RandomVerticalFlip(),
+    T.ToTensor(),
+    T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+# TODO Find better mean, std_dev for Normalization
+# TODO See: https://github.com/ugent-korea/pytorch-unet-segmentation/blob/master/src/pre_processing.py
